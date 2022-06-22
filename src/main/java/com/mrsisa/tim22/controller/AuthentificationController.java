1b65f2281ca99e6272a96d980f30bf665396a51d
SteX266 <stefan.milosevic.e14@gmail.com>
2022-06-22 18:56:24 +0200

    idk

--- src/main/java/com/mrsisa/tim22/controller/AuthentificationController.java
+++ src/main/java/com/mrsisa/tim22/controller/AuthentificationController.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"*"})
 @RequestMapping(value = "/auth")
 public class AuthentificationController {
 
